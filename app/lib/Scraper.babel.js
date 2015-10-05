aaf3ec25bd689f5c72958fa5d74d4f3666b94617
Sean Prasertsit <seanprasertsit@gmail.com>
2015-10-05 14:17:00 -0700

    Started work on scraper

---
+++ app/lib/Scraper.babel.js
@@ -0,0 +1,37 @@
+import request from 'request';
+import cheerio from 'cheerio';
+import mongodb from 'mongodb';
+import bunyan from 'bunyan';
+
+const client = mongodb.MongoClient;
+const log = bunyan.createLogger({name: "gophrazy"});
+
+const connection = 'mongodb://localhost:27017/gophrazy';
+const db = client.connect(connection, (err, db) {
+  if (err !== null) {
+    log.fatal("Failed to connect to database.");
+  }
+});
+
+// TODO: $('table:nth-of-type(4) > tbody > tr > td > font').text()
+
+const Scraper = {
+  isPhrase: (value) => {
+    return value.length > 1;
+  },
+  elScrape: (url, tag) => {
+    request(url, (error, response, html) {
+      if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        let phraseArray = [];
+        $('table:nth-of-type(4) > tbody > tr > td').each(() => {
+          $(this).find('font').each(() => {
+            phraseArray = $(this).text().split(' ');
+            const phrases = phraseArray.filter(Scraper.isPhrase);
+          })
+        });
+      }
+    })
+  },
+};
+fd
