4762241c37e377032a3380279f5ca70f4a5eb2dd
Sean Prasertsit <seanprasertsit@gmail.com>
2015-10-12 09:58:37 -0700

    Create basic angular app to test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,21 @@
 import gulp from 'gulp';
 import jade from 'gulp-jade';
+import stylus from 'gulp-stylus';
+import nib from 'nib';
 
 gulp.task('templates', () => {
   return gulp.src('assets/templates/index.jade')
     .pipe(jade())
     .pipe(gulp.dest('public'));
 });
+
+gulp.task('stylesheets', () => {
+  return gulp.src('assets/stylesheets/*.styl')
+    .pipe(stylus({
+      compress: true,
+      use: nib(),
+    }))
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
+gulp.task('default', ['templates', 'stylesheets']);
