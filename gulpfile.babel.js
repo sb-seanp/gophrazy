3a5a13e605d4cf111b231496d132f56d1b038b1c
Sean Prasertsit <seanprasertsit@gmail.com>
2015-10-14 14:01:16 -0700

    Removed public folder and added app/dist and changed buildfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,19 +3,35 @@ import jade from 'gulp-jade';
 import stylus from 'gulp-stylus';
 import nib from 'nib';
 
+const paths = {
+  dist: {
+    public: 'app/dist/',
+    stylesheets: 'app/dist/stylesheets/',
+    javascript: 'app/dist/javascript/',
+  },
+  src: {
+    templates: 'app/src/assets/templates/',
+    stylesheets: 'app/src/assets/stylesheets/',
+    javascript: 'app/src/assets/javascript/',
+    components: 'app/src/components/',
+  },
+};
+
 gulp.task('templates', () => {
-  return gulp.src('assets/templates/index.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('public'));
+  return gulp.src(paths.src.templates + 'index.jade')
+    .pipe(jade({
+      pretty: true,
+    }))
+    .pipe(gulp.dest(paths.dist.public));
 });
 
 gulp.task('stylesheets', () => {
-  return gulp.src('assets/stylesheets/*.styl')
+  return gulp.src(paths.src.stylesheets + '*.styl')
     .pipe(stylus({
       compress: true,
       use: nib(),
     }))
-    .pipe(gulp.dest('public/stylesheets'));
+    .pipe(gulp.dest(paths.dist.stylesheets));
 });
 
 gulp.task('default', ['templates', 'stylesheets']);
